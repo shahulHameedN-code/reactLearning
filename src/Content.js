024bf1ca9fd010b3b6f46e4b6889cbc19fc203b8
shahul <shahulh32@gmail.com>
2024-07-09 14:49:10 +0530

    to do list

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,47 @@
 import React from 'react'
 import { useState } from 'react';
-
+import { FaTrashAlt } from 'react-icons/fa';
 const Content = () => {
 
-  const [name, setName]=useState("give")
-  function handleNameChange(){
-    const names=['give','earn','grow']
-    const randomInt=Math.floor(Math.random()*3)
-    setName(names[randomInt])
+
+  const [items,setItems]=useState([{id:1,checked:true,description:"study react"},
+    {id:2,checked:false,description:"code"},
+    {id:3,checked:true,description:"sleep"}
+  ]);
+
+const handleCheck=(id) =>{
+  const itemsList=items.map((item)=> 
+  item.id===id ? {...item, checked:!item.checked}:item)
+  setItems(itemsList)
+}
+const handleDelete=(id)=>{
+  const itemList=items.filter((item)=>item.id!==id)
+  setItems(itemList)
+}
+
+const renderContent= ()=>{
+  if(items.length){
+ return <ul>
+  {items.map((item)=>(
+  <li className='item' key={item.id}>
+    <input type="checkbox" onChange={()=>handleCheck(item.id)} checked={item.checked} />
+    <label style={(item.checked)?{textDecoration: 'line-through'} :null}>{item.description}</label>
+    <FaTrashAlt
+    role='button'
+    tabIndex="0"
+    onClick={()=>handleDelete(item.id)}/>
+  </li>
+  ))}
+</ul>
   }
+  else{
+    return <p> Your list is empty</p>
+  }
+}
 
   return (
     <main>
-      <p>let's {name} money</p>
-      <button onClick={handleNameChange}>subscribe</button>
+      {renderContent()}
     </main>
   )
 }
